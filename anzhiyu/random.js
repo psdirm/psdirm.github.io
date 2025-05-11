947681b6f9ae9bf70464ea11f724659565455e48
13731725820 <psdirm@qq.com>
2025-05-11 18:03:02 +0800

    Site updated: 2025-05-11 18:03:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/09/hello-world/"];function toRandomPost(){
+var posts=["2025/04/09/hello-world/","1919/03/01/孔乙己/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
